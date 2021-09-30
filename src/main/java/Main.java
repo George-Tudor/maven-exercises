b6f1a1cc59bd89672b0f579b35fc6348cca680e5
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-29 19:05:41 -0500

    maven warmup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tell me something interesting: ");
+        String userInput = sc.nextLine();
+
+
 
     }
 }
